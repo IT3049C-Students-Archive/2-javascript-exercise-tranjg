505a2595b54e9b0940d4f9588804401db945a15b
tranjg <tranjg@mail.uc.edu>
2020-09-01 19:01:46 -0400

    finished test filterOut

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -18,7 +18,7 @@ function sum(arr) {
 // Filter: Should filter out all instances of a value from an array
 // Example: filtering 2 out of [1,2,3,5,6,2,4,2] should return [1,3,5,6,4]
 function filterOut(arr, item) {
-  return ;
+  return arr.filter(i => i !== item);
 }
 
 // Append: you should be able to add an item to the end of an array
